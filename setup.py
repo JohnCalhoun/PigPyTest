645b564c3e35e107f1b4f1a0c7c8ba971d896e99
John M Calhoun <johnmcalhoun123@gmail.com>
2016-08-30 14:56:12 -0500

    initial structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='PigPyTest',
+      version='0.1',
+      description='Python unittesting of Pig scripts',
+      url='https://github.com/JohnCalhoun/PigPyTest',
+      author='John M Calhoun',
+      author_email='programing@johnmcalhoun.com',
+      license='APACHE v2',
+      packages=['PigPyTest'],
+      zip_safe=False)
